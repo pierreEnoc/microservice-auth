392d613176d31f0f0c9c4b3b7521a95812ba7fd8
Pierre enoc <pierre.enoc@gympass.com>
2021-02-18 18:27:30 -0300

    Authcontroller

--- src/main/java/br/com/pierredev/auth/AuthApplication.java
+++ src/main/java/br/com/pierredev/auth/AuthApplication.java
@@ -1,13 +1,61 @@
 package br.com.pierredev.auth;
 
+import br.com.pierredev.auth.entity.Permission;
+import br.com.pierredev.auth.entity.User;
+import br.com.pierredev.auth.repositories.PermissionRepository;
+import br.com.pierredev.auth.repositories.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class AuthApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthApplication.class, args);
+}
+
+	@Bean
+	CommandLineRunner init(UserRepository userRepository, PermissionRepository permissionRepository,
+						   BCryptPasswordEncoder passwordEncoder) {
+		return args -> {
+			initUsers(userRepository, permissionRepository, passwordEncoder);
+		};
+
 	}
 
-}
+	private void initUsers(UserRepository userRepository, PermissionRepository permissionRepository,
+						   BCryptPasswordEncoder passwordEncoder) {
+
+		Permission permission = null;
+		Permission findPermission = permissionRepository.findByDescription("Admin");
+		if (findPermission == null) {
+			permission = new Permission();
+			permission.setDescription("Admin");
+			permission = permissionRepository.save(permission);
+		} else {
+			permission = findPermission;
+		}
+
+		User admin = new User();
+		admin.setUserName("pierre");
+		admin.setAccountNonExpired(true);
+		admin.setAccountNonLocked(true);
+		admin.setCredentialsNonExpired(true);
+		admin.setEnabled(true);
+		admin.setPassword(passwordEncoder.encode("123456"));
+		admin.setPermissions(Arrays.asList(permission));
+
+		User find = userRepository.findByUserName("pierre");
+		if (find == null) {
+			userRepository.save(admin);
+		}
+	}
+	}
